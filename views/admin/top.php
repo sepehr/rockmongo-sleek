98009c4a548210fb69052aef3d3298cf8d6d46bc
Sepehr Lajevardi <me@sepehr.ws>
2013-06-21 17:42:23 +0430

    Reordered top menu, removed separators.

--- views/admin/top.php
+++ views/admin/top.php
@@ -13,12 +13,29 @@ function showServerMenu(link) {
 
 <div class="top">
 	<div class="left">
+		<?php if(!is_null($isMaster)): ?>
+			<?php if($isMaster):?>
+			<a href="<?php h(url("server.replication")); ?>" target="right" title="<?php hm("master"); ?>">
+				<?php hm("master"); ?>
+			</a>
+			<?php else:?>
+			<a href="<?php h(url("server.replication")); ?>" target="right" title="<?php hm("slave"); ?>">
+				<?php hm("slave"); ?>
+			</a>
+			<?php endif;?>
+		<?php endif;?>
+
+		<a href="#" onclick="showServerMenu(this);return false;">
+			<?php hm("tools");?> <span style="font-size:11px">▼</span>
+		</a>
+
 		<select name="host" onchange="window.parent.location='<?php h(url("admin.changeHost")); ?>&index='+this.value" title="<?php hm("switch_hosts"); ?>">
 		<?php foreach ($servers as $index => $server):?>
-		<option value="<?php h($index);?>" <?php if($index == $serverIndex): ?>selected="selected"<?php endif;?>><?php h(isset($server["mongo_name"]) ? $server["mongo_name"] : "");?></option>
+		<option value="<?php h($index);?>" <?php if($index == $serverIndex): ?>selected="selected"<?php endif;?>>
+			<?php h(isset($server["mongo_name"]) ? $server["mongo_name"] : "");?>
+		</option>
 		<?php endforeach; ?>
 		</select>
-		| <a href="#" onclick="showServerMenu(this);return false;"><?php hm("tools");?> <span style="font-size:11px">▼</span></a> <?php if(!is_null($isMaster)): ?>| <?php if($isMaster):?><a href="<?php h(url("server.replication")); ?>" target="right" title="<?php hm("master"); ?>"><?php hm("master"); ?></a><?php else:?><a href="<?php h(url("server.replication")); ?>" target="right" title="<?php hm("slave"); ?>"><?php hm("slave"); ?></a><?php endif;?><?php endif;?>
 	</div>
 
 	<div class="right">
