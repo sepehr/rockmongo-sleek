4899bf478eb4ecb5b8a135f5f9e101ae46527ece
Sepehr Lajevardi <me@sepehr.ws>
2014-06-09 08:40:58 +0430

    Login page style update.

--- views/login/index.php
+++ views/login/index.php
@@ -36,33 +36,33 @@ function r_show_more_options(btn) {
 	<input type="hidden" name="more" id="show_more_options" value="<?php echo $moreOptions; ?>"/>
 	<table>
 		<tr class="separator">
-			<td colspan="2"><strong>Login</strong></td>
+			<td colspan="2">Login</td>
 		</tr>
 		<tr>
 			<td width="100">Host</td>
 			<td><?php render_select_hosts("host", $hostIndex); ?></td>
 		</tr>
 		<tr>
-			<td nowrap><?php hm("username"); ?>:</td>
+			<td nowrap><?php hm("username"); ?></td>
 			<td><input type="text" name="username" value="<?php echo $username;?>" style="width:150px"/></td>
 		</tr>
 		<tr>
-			<td><?php hm("password"); ?>:</td>
+			<td><?php hm("password"); ?></td>
 			<td><input type="password" name="password" style="width:150px"/></td>
 		</tr>
 		<tr class="separator">
-			<td colspan="2"><strong>Non-Admins</strong></td>
+			<td colspan="2">Non-Admins</td>
 		</tr>
 		<tr>
-			<td valign="top">DB Name(s):</td>
+			<td valign="top">DB Name(s)</td>
 			<td><input type="text" name="db" style="width:150px" value="<?php h($db); ?>"/><br/></td>
 		</tr>
-		<tr>
+		<tr class="more">
 			<td colspan="2"><a href="#" onclick="return r_show_more_options(this)" id="more_options_btn">More &raquo;</a></td>
 		</tr>
 		<tbody style="display:none" id="more_options">
 		<tr>
-			<td><?php hm("language"); ?>:</td>
+			<td><?php hm("language"); ?></td>
 			<td><select name="lang" style="width:150px">
 			<?php foreach ($languages as $code => $lang):?>
 			<option value="<?php h($code);?>" <?php if(x("lang") == $code || (x("lang") ==""&&__LANG__==$code)): ?>selected="selected"<?php endif;?> ><?php h($lang); ?></option>
