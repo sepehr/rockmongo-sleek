a5bcbc39072da88cf4281a5bd84a361da74ccc65
Sepehr Lajevardi <me@sepehr.ws>
2014-06-12 10:46:27 +0430

    Server process list style.

--- views/server/processlist.php
+++ views/server/processlist.php
@@ -4,9 +4,10 @@
 
 <table bgcolor="#cccccc" cellpadding="2" cellspacing="1" width="600" class="table-main">
 	<tr>
-		<th colspan="10"><?php hm("processlist"); ?> (db.$cmd.sys.inprog.find({$all:1}))</th>
+		<th colspan="10"><?php hm("processlist"); ?> <span class="code">(db.$cmd.sys.inprog.find({$all:1}))</span></th>
 	</tr>
-	<tr bgcolor="#cfffff">
+
+	<tr bgcolor="#efefef">
 		<?php foreach (array(
 			"id"             =>"ID",
 			"desc"           => "Description",
@@ -17,12 +18,13 @@
 			"secs_running"   => "SecsRunning",
 			"op"             => "Operation",
 			"ns"             => "NameSpace"
-			) as $param => $desc):?>
-		<td><?php h($desc); ?></td>
+			) as $param => $desc): ?>
+		<td style="padding:5px; text-align:center"><?php h($desc); ?></td>
 		<?php endforeach; ?>
 	</tr>
+
 	<?php foreach ($progs as $prog):?>
-	<tr bgcolor="#fffeee">
+	<tr bgcolor="#fff">
 		<?php foreach (array(
 			"opid"           =>"ID",
 			"desc"           => "Description",
@@ -33,11 +35,16 @@
 			"secs_running"   => "SecsRunning",
 			"op"             => "Operation",
 			"ns"             => "NameSpace",
-			) as $param => $desc):?>
-		<td valign="top" <?php if(isset($prog["query"])&&$param=="opid"): ?>rowspan="2"<?php endif; ?>>
+			) as $param => $desc): ?>
+		<td class="code" valign="top" <?php if(isset($prog["query"])&&$param=="opid"): ?>rowspan="2"<?php endif; ?>>
 			<?php if(isset($prog[$param])):?>
 				<?php if($param=="opid"):?>
-					<?php h($prog["opid"]);?><?php if($prog["opid"]>1):?> [<a href="<?php h(url("server.killOp", array("opid"=>$prog["opid"]))); ?>" onclick="return window.confirm('<?php hm("killop"); ?> \'<?php h($prog["opid"]);?>\'?')"><?php hm("kill"); ?></a>]<?php endif;?>
+					<?php h($prog["opid"]);?>
+					<?php if($prog["opid"]>1):?>
+						<a class="op-kill" href="<?php h(url("server.killOp", array("opid"=>$prog["opid"]))); ?>" onclick="return window.confirm('<?php hm("killop"); ?> \'<?php h($prog["opid"]);?>\'?')">
+							[<?php hm("kill"); ?>]
+						</a>
+					<?php endif;?>
 				<?php
 				else:
 					if($prog[$param] == "(NONE)" ||$prog[$param] == "none"){}else{ h($prog[$param]); }
@@ -48,8 +55,8 @@
 		<?php endforeach;?>
 	</tr>
 	<?php if(isset($prog["query"])):?>
-	<tr bgcolor="#fffeee">
-		<td colspan="10"><strong><?php hm("query"); ?></strong><br/>-----<br/><?php h($prog["query"]); ?></td>
+	<tr bgcolor="#fff">
+		<td class="code" colspan="10"><strong><?php hm("query"); ?></strong><br/>-----<br/><?php h($prog["query"]); ?></td>
 	</tr>
 	<?php endif; ?>
 	<?php endforeach; ?>
