f32214d5fb60e6cbd55a5f11e7bfadd38c0f54c1
Sepehr Lajevardi <me@sepehr.ws>
2014-06-19 07:45:48 +0430

    Collection stats page update.

--- views/collection/collectionStats.php
+++ views/collection/collectionStats.php
@@ -1,5 +1,6 @@
 <h3><?php render_navigation($db,$collection,false); ?> &raquo; <?php hm("statistics"); ?> [<a href="<?php h($_SERVER["REQUEST_URI"]);?>"><?php hm("refresh"); ?></a>]</h3>
 
+<div class="content-wrapper">
 <table bgcolor="#cccccc" width="600" class="table-main" cellpadding="2" cellspacing="1">
 	<tr bgcolor="#cfffff">
 		<th colspan="2" class="code">db.<?php h($collection); ?>.stats()</th>
@@ -7,8 +8,9 @@
 
 <?php foreach ($stats as $name => $stat):?>
 	<tr bgcolor="#ffffff">
-		<td width="150" bgcolor="#fffeee" valign="top"><?php h($name);?></td>
-		<td><?php
+		<td width="150" bgcolor="#fffeee" valign="top" class="code"><?php h($name);?></td>
+		<td class="code">
+		<?php
 			if (is_array($stat)) {
 				h("<xmp>" . var_export($stat, true) . "</xmp>");
 			}
@@ -18,7 +20,8 @@
 				}
 				h($stat);
 			}
-			?></td>
+			?>
+		</td>
 	</tr>
 <?php endforeach; ?>
 
@@ -36,3 +39,4 @@
 	<?php endforeach; ?>
 <?php endif;?>
 </table>
+</div>
