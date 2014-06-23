506cc421b5213b19a00ecf8f4e906bee972c9e64
Sepehr Lajevardi <me@sepehr.ws>
2014-06-23 10:04:00 +0430

    Updated collection import page.

--- views/db/dbImport.php
+++ views/db/dbImport.php
@@ -1,7 +1,7 @@
 <h3><?php render_navigation($db); ?> &raquo; <?php hm("import"); ?></h3>
 
 <?php if (isset($error)):?> <p class="error"><?php h($error);?></p><?php endif; ?>
-<?php if (isset($message)):?> 
+<?php if (isset($message)):?>
 <p class="message"><?php h($message);?></p>
 <script language="javascript">
 window.parent.frames["left"].location.reload();
@@ -12,13 +12,13 @@ window.parent.frames["left"].location.reload();
 
 <form method="post" enctype="multipart/form-data">
 <input type="hidden" name="format" value="js"/>
-JS File: <input type="file" style="width:400px" name="json"/><br/>
+<input type="file" style="width:400px" name="json"/><br/><br/>
 <input type="submit" value="<?php hm("import"); ?>"/>
 </form>
 
-<hr style="margin:20px 0px"/>
+<hr style="border:1px solid #ccc;margin:20px 0px"/>
 <?php if (isset($error2)):?> <p class="error"><?php h($error2);?></p><?php endif; ?>
-<?php if (isset($message2)):?> 
+<?php if (isset($message2)):?>
 <p class="message"><?php h($message2);?></p>
 <script language="javascript">
 window.parent.frames["left"].location.reload();
@@ -29,6 +29,6 @@ window.parent.frames["left"].location.reload();
 <form method="post" enctype="multipart/form-data">
 <input type="hidden" name="format" value="json"/>
 Import to collection name:<input type="text" name="collection"/><br/>
-JSON File: <input type="file" style="width:400px" name="json"/><br/>
+<input type="file" style="width:400px" name="json"/><br/><br/>
 <input type="submit" value="<?php hm("import"); ?>"/>
 </form>
